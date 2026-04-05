15c3c7c5ab2fd4710dac0cd4b5cd55e663df5122
Tien Le <nhocconan@gmail.com>
2026-04-05 00:41:42 +0000

    exp#6997: exp_6997_4h_donchian20_1d_ema_vol_v1

--- strategy.py
+++ strategy.py
@@ -23,7 +23,7 @@ VOL_BASE_THRESHOLD = 2.0
 SIGNAL_SIZE = 0.25
 ATR_PERIOD = 14
 ATR_STOP_MULTIPLIER = 2.5
-MAX_HOLD_BARS = 30  # ~5 months (4h bars)
+MAX_HOLD_BARS = 30  # ~2.5 months (4h bars)
 EMA_PERIOD = 50
 
 def generate_signals(prices):
@@ -128,5 +128,3 @@ def generate_signals(prices):
             signals[i] = position * SIGNAL_SIZE
     
     return signals
-
-</think>
