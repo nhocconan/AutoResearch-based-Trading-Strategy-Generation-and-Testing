fdec7c93d8e6ee51e8eff80647c8906b1809817a
Tien Le <nhocconan@gmail.com>
2026-04-06 16:51:55 +0000

    exp#15289: 4h_donchian20_1d_ema_vol_v1

--- strategy.py
+++ strategy.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "4h_donchian20_1d_ema200_vol_v1"
+name = "4h_donchian20_1d_ema_vol_v1"
 timeframe = "4h"
 leverage = 1.0
 
