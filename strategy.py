643f342acff6bd576155daa3b091b12e5a64dcf7
Tien Le <nhocconan@gmail.com>
2026-04-07 21:28:24 +0000

    exp#20941: 4h_donchian_breakout_1d_trend_volume_v2

--- strategy.py
+++ strategy.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "4h_donchian_breakout_1d_trend_volume_v1"
+name = "4h_donchian_breakout_1d_trend_volume_v2"
 timeframe = "4h"
 leverage = 1.0
 
