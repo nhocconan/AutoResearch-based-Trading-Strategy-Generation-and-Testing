68f492ed2f28731a5f0ba7fdcdb1a73bf8458449
Tien Le <nhocconan@gmail.com>
2026-04-08 02:48:51 +0000

    exp#22301: 4h_daily_pivot_breakout_1d_trend_volume_v7

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "4h_daily_pivot_breakout_1d_trend_volume_v6"
+name = "4h_daily_pivot_breakout_1d_trend_volume_v7"
 timeframe = "4h"
 leverage = 1.0
 
