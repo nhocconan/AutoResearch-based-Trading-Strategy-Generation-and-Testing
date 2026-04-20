f734d7d165f4ca6d71f5ced5cbfa20a971fab1f3
Tien Le <nhocconan@gmail.com>
2026-04-20 12:32:55 +0000

    exp#67401: 4h_1d_Camarilla_R1S1_Breakout_VolumeTrend_v3

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "4h_1d_Camarilla_R1S1_Breakout_VolumeTrend_v1"
+name = "4h_1d_Camarilla_R1S1_Breakout_VolumeTrend_v3"
 timeframe = "4h"
 leverage = 1.0
 
