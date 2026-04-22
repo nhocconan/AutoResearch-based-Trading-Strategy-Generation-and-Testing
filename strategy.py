80d17db30b7b71d65b6aa7fa0d59a5240d6763f4
Tien Le <nhocconan@gmail.com>
2026-04-22 12:06:36 +0000

    exp#75628: 12H_Camarilla_R1_S1_Breakout_1dEMA34_Trend_Volume

--- strategy.py
+++ strategy.py
@@ -5,7 +5,7 @@ from mtf_data import get_htf_data, align_htf_to_ltf
 
 def generate_signals(prices):
     n = len(prices)
-    if n < 40:
+    if n < 50:
         return np.zeros(n)
     
     close = prices['close'].values
