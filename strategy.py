b39734b78be2b92b0cb357c2cb804bab3b2b8b70
Tien Le <nhocconan@gmail.com>
2026-04-27 06:05:28 +0000

    exp#97098: 1d_Camarilla_R3S3_R4S4_FadeBreakout_1d1wEMA34_Trend_VolumeSpike_v3

--- strategy.py
+++ strategy.py
@@ -44,7 +44,7 @@ def generate_signals(prices):
     r4 = pivot + range_ * 1.5
     s4 = pivot - range_ * 1.5
     
-    # Align levels to 4h timeframe
+    # Align levels to 1d timeframe (already aligned via function)
     r3_aligned = align_htf_to_ltf(prices, df_1d, r3)
     s3_aligned = align_htf_to_ltf(prices, df_1d, s3)
     r4_aligned = align_htf_to_ltf(prices, df_1d, r4)
@@ -119,6 +119,6 @@ def generate_signals(prices):
     
     return signals
 
-name = "4h_Camarilla_R3S3_R4S4_FadeBreakout_1d1wEMA34_Trend_VolumeSpike_v3"
-timeframe = "4h"
+name = "1d_Camarilla_R3S3_R4S4_FadeBreakout_1d1wEMA34_Trend_VolumeSpike_v3"
+timeframe = "1d"
 leverage = 1.0
