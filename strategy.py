3841bc054dbc44d3a9082e254153a15d7af653ff
Tien Le <nhocconan@gmail.com>
2026-04-27 12:31:24 +0000

    exp#98867: 6h_Donchian20_1dEMA34_Volume_Trend

--- strategy.py
+++ strategy.py
@@ -99,6 +99,6 @@ def generate_signals(prices):
     
     return signals
 
-name = "4h_Donchian20_1dEMA34_Volume_Trend"
-timeframe = "4h"
+name = "6h_Donchian20_1dEMA34_Volume_Trend"
+timeframe = "6h"
 leverage = 1.0
