859411fd483966f63c7d9b462e1e89b873a6d539
Tien Le <nhocconan@gmail.com>
2026-05-07 05:30:08 +0000

    exp#132942: 12h_Camarilla_R1_S1_Breakout_1dEMA34_VolumeSpike_v1

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R1_S1_Breakout_1dEMA34_VolumeSpike_v13"
-timeframe = "4h"
+name = "12h_Camarilla_R1_S1_Breakout_1dEMA34_VolumeSpike_v1"
+timeframe = "12h"
 leverage = 1.0
 
 import numpy as np
