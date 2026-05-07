3edd64f52e4f07a70400f2bc939427e63587580a
Tien Le <nhocconan@gmail.com>
2026-05-07 09:08:00 +0000

    exp#133936: 12h_Camarilla_R1S1_Breakout_1dTrend_Volume

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "1h_Camarilla_R1S1_Breakout_1dTrend_Volume"
-timeframe = "1h"
+name = "12h_Camarilla_R1S1_Breakout_1dTrend_Volume"
+timeframe = "12h"
 leverage = 1.0
 
 import numpy as np
@@ -16,10 +16,6 @@ def generate_signals(prices):
     high = prices['high'].values
     low = prices['low'].values
     volume = prices['volume'].values
-    
-    # Pre-compute session filter (08-20 UTC)
-    hours = prices.index.hour
-    in_session = (hours >= 8) & (hours <= 20)
     
     # Get 1d data for trend filter (EMA50) and Camarilla calculation
     df_1d = get_htf_data(prices, '1d')
@@ -46,7 +42,7 @@ def generate_signals(prices):
     r1 = close_1d_shifted + camarilla_width  # R1 level
     s1 = close_1d_shifted - camarilla_width  # S1 level
     
-    # Align Camarilla levels to 1h timeframe
+    # Align Camarilla levels to 12h timeframe
     r1_aligned = align_htf_to_ltf(prices, df_1d, r1)
     s1_aligned = align_htf_to_ltf(prices, df_1d, s1)
     
@@ -70,24 +66,18 @@ def generate_signals(prices):
                 position = 0
             continue
         
-        if not in_session[i]:
-            if position != 0:
-                signals[i] = 0.0
-                position = 0
-            continue
-        
         if position == 0:
             # Long: price breaks above R1 level, uptrend (price > EMA50), volume confirmation
             if (close[i] > r1_aligned[i] and 
                 close[i] > ema_50_1d_aligned[i] and 
                 volume_ratio[i] > 2.0):
-                signals[i] = 0.20
+                signals[i] = 0.25
                 position = 1
             # Short: price breaks below S1 level, downtrend (price < EMA50), volume confirmation
             elif (close[i] < s1_aligned[i] and 
                   close[i] < ema_50_1d_aligned[i] and 
                   volume_ratio[i] > 2.0):
-                signals[i] = -0.20
+                signals[i] = -0.25
                 position = -1
         elif position == 1:
             # Exit long: price breaks below S1 level (reversal signal)
@@ -95,13 +85,13 @@ def generate_signals(prices):
                 signals[i] = 0.0
                 position = 0
             else:
-                signals[i] = 0.20
+                signals[i] = 0.25
         elif position == -1:
             # Exit short: price breaks above R1 level (reversal signal)
             if close[i] > r1_aligned[i]:
                 signals[i] = 0.0
                 position = 0
             else:
-                signals[i] = -0.20
+                signals[i] = -0.25
     
     return signals
