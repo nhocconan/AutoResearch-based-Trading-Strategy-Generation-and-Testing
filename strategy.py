b259bbaab13755234e7d98109c812dfdabeb21f5
Tien Le <nhocconan@gmail.com>
2026-05-08 08:14:36 +0000

    exp#138637: 4h_Camarilla_R3_S3_Breakout_1dTrend_Volume_v2

--- strategy.py
+++ strategy.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "12h_Camarilla_R3_S3_Breakout_1dTrend_Volume_v1"
-timeframe = "12h"
+name = "4h_Camarilla_R3_S3_Breakout_1dTrend_Volume_v2"
+timeframe = "4h"
 leverage = 1.0
 
 def generate_signals(prices):
@@ -33,6 +33,8 @@ def generate_signals(prices):
     # Calculate Camarilla levels from previous day
     R3 = np.zeros(len(close_1d))
     S3 = np.zeros(len(close_1d))
+    R4 = np.zeros(len(close_1d))
+    S4 = np.zeros(len(close_1d))
     
     for i in range(1, len(close_1d)):
         high_prev = high_1d[i-1]
@@ -43,10 +45,14 @@ def generate_signals(prices):
         C = close_prev + (range_val * 1.1 / 6)
         R3[i] = C + (range_val * 1.1 / 2)
         S3[i] = C - (range_val * 1.1 / 2)
+        R4[i] = C + (range_val * 1.1)
+        S4[i] = C - (range_val * 1.1)
     
-    # Align Camarilla levels to 12h timeframe
+    # Align Camarilla levels to 4h timeframe
     R3_aligned = align_htf_to_ltf(prices, df_1d, R3)
     S3_aligned = align_htf_to_ltf(prices, df_1d, S3)
+    R4_aligned = align_htf_to_ltf(prices, df_1d, R4)
+    S4_aligned = align_htf_to_ltf(prices, df_1d, S4)
     
     # Volume spike: current volume > 1.5x 20-period average
     vol_ma20 = pd.Series(volume).rolling(window=20, min_periods=20).mean().values
