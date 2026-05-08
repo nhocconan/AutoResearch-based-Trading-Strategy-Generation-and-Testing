da81076c820770144337ce066ad96d785224a8c7
Tien Le <nhocconan@gmail.com>
2026-05-08 04:45:36 +0000

    exp#137786: 4h_Camarilla_R1_S1_Breakout_1dTrend_Volume

--- strategy.py
+++ strategy.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "12h_Camarilla_R1_S1_Breakout_1dTrend_Volume"
-timeframe = "12h"
+name = "4h_Camarilla_R1_S1_Breakout_1dTrend_Volume"
+timeframe = "4h"
 leverage = 1.0
 
 def generate_signals(prices):
@@ -44,7 +44,7 @@ def generate_signals(prices):
     R1 = pivot + (range_hl * 1.1 / 6)
     S1 = pivot - (range_hl * 1.1 / 6)
     
-    # Align Camarilla levels to 12h timeframe
+    # Align Camarilla levels to 4h timeframe
     R1_aligned = align_htf_to_ltf(prices, df_1d, R1)
     S1_aligned = align_htf_to_ltf(prices, df_1d, S1)
     pivot_aligned = align_htf_to_ltf(prices, df_1d, pivot)
@@ -79,10 +79,10 @@ def generate_signals(prices):
                          volume_spike[i]
             
             if long_cond:
-                signals[i] = 0.25
+                signals[i] = 0.30
                 position = 1
             elif short_cond:
-                signals[i] = -0.25
+                signals[i] = -0.30
                 position = -1
         elif position == 1:
             # Long exit: close below pivot (mean reversion to mean)
@@ -90,13 +90,13 @@ def generate_signals(prices):
                 signals[i] = 0.0
                 position = 0
             else:
-                signals[i] = 0.25
+                signals[i] = 0.30
         elif position == -1:
             # Short exit: close above pivot (mean reversion to mean)
             if close[i] > pivot_aligned[i]:
                 signals[i] = 0.0
                 position = 0
             else:
-                signals[i] = -0.25
+                signals[i] = -0.30
     
     return signals
