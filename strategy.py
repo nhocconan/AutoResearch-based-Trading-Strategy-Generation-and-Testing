09ed91bd67d182454600bbd9e595582573ad2442
Tien Le <nhocconan@gmail.com>
2026-05-08 04:48:51 +0000

    exp#137797: 4h_Camarilla_R1_S1_Breakout_1dTrend_Volume

--- strategy.py
+++ strategy.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "12h_Camarilla_R1_S1_Breakout_1dTrend_Volume_Strict"
-timeframe = "12h"
+name = "4h_Camarilla_R1_S1_Breakout_1dTrend_Volume"
+timeframe = "4h"
 leverage = 1.0
 
 def generate_signals(prices):
@@ -47,7 +47,7 @@ def generate_signals(prices):
     R1 = pivot + (range_hl * 1.1 / 6)
     S1 = pivot - (range_hl * 1.1 / 6)
     
-    # Align Camarilla levels to 12h timeframe
+    # Align Camarilla levels to 4h timeframe
     R1_aligned = align_htf_to_ltf(prices, df_1d, R1)
     S1_aligned = align_htf_to_ltf(prices, df_1d, S1)
     pivot_aligned = align_htf_to_ltf(prices, df_1d, pivot)
