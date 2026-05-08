a69560c6ec9100290664ffa44c522987997d0491
Tien Le <nhocconan@gmail.com>
2026-05-08 05:19:09 +0000

    exp#137920: 4h_Camarilla_R3S3_Breakout_1dTrend_Volume_02

--- strategy.py
+++ strategy.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "6h_Camarilla_R3S3_Breakout_1dTrend_Volume"
-timeframe = "6h"
+name = "4h_Camarilla_R3S3_Breakout_1dTrend_Volume_02"
+timeframe = "4h"
 leverage = 1.0
 
 def generate_signals(prices):
