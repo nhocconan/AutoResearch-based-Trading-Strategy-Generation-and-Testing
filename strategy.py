647b72d21f98001de983ab4e6bfcce0afb4cc6fb
Tien Le <nhocconan@gmail.com>
2026-05-08 09:29:52 +0000

    exp#138961: 4h_Camarilla_R1_S1_Breakout_1dTrend_VolumeFilter

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from mtf_data import get_htf_data, align_htf_to_ltf
 
-name = "4h_Camarilla_R1_S1_Breakout_1dTrend_VolumeFilter_v4"
+name = "4h_Camarilla_R1_S1_Breakout_1dTrend_VolumeFilter"
 timeframe = "4h"
 leverage = 1.0
 
