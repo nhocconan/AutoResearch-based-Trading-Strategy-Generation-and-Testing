458d0ca31a8177458ca3d1b0dc505b2f4b7b12d9
Tien Le <nhocconan@gmail.com>
2026-05-11 09:26:40 +0000

    exp#153141: 4h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Controlled

--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Enhanced"
+name = "4h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Controlled"
 timeframe = "4h"
 leverage = 1.0
 
