778f61385f10a2b08bb6051c1eb58356f0c50234
Tien Le <nhocconan@gmail.com>
2026-05-11 09:21:58 +0000

    exp#153122: 12h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Controlled

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Controlled"
-timeframe = "4h"
+name = "12h_Camarilla_R3_S3_Breakout_1dEMA34_VolumeSpike_Controlled"
+timeframe = "12h"
 leverage = 1.0
 
 import numpy as np
@@ -32,13 +32,13 @@ def generate_signals(prices):
     r3 = close_1d + hl_range * 1.25
     s3 = close_1d - hl_range * 1.25
     
-    # Align Camarilla levels to 4h timeframe
+    # Align Camarilla levels to 12h timeframe
     r3_aligned = align_htf_to_ltf(prices, df_1d, r3)
     s3_aligned = align_htf_to_ltf(prices, df_1d, s3)
     
     # Volume filter: 50-period EMA for higher threshold
     vol_ema50 = pd.Series(volume).ewm(span=50, min_periods=50, adjust=False).mean().values
-    volume_ok = volume > vol_ema50 * 2.5  # Further increased threshold to reduce trades
+    volume_ok = volume > vol_ema50 * 2.5
     
     # Fixed position size to avoid churn
     position_size = 0.25
