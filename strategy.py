b0e853018b6e36fcff90c11e8360b6e50bdd9712
Tien Le <nhocconan@gmail.com>
2026-05-11 22:10:46 +0000

    exp#155623: 4h_Camarilla_R1S1_Breakout_1dTrend_Volume_Spike

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "12h_Camarilla_R1S1_Breakout_1dTrend_Volume"
-timeframe = "12h"
+name = "4h_Camarilla_R1S1_Breakout_1dTrend_Volume_Spike"
+timeframe = "4h"
 leverage = 1.0
 
 import numpy as np
@@ -9,7 +9,7 @@ from mtf_data import get_htf_data, align_htf_to_ltf
 
 def generate_signals(prices):
     n = len(prices)
-    if n < 100:
+    if n < 200:
         return np.zeros(n)
     
     close = prices['close'].values
@@ -36,7 +36,7 @@ def generate_signals(prices):
     camarilla_r1 = prev_close_1d + (prev_high_1d - prev_low_1d) * 1.083
     camarilla_s1 = prev_close_1d - (prev_high_1d - prev_low_1d) * 1.083
     
-    # Align to 12h timeframe (use previous day's levels)
+    # Align to 4h timeframe (use previous day's levels)
     camarilla_r1_aligned = align_htf_to_ltf(prices, df_1d, camarilla_r1)
     camarilla_s1_aligned = align_htf_to_ltf(prices, df_1d, camarilla_s1)
     
@@ -44,9 +44,9 @@ def generate_signals(prices):
     ema_1d = pd.Series(close_1d).ewm(span=34, adjust=False, min_periods=34).mean().values
     ema_1d_aligned = align_htf_to_ltf(prices, df_1d, ema_1d)
     
-    # Volume filter (20-period average)
+    # Volume spike filter (20-period average)
     vol_ma = pd.Series(volume).rolling(window=20, min_periods=20).mean().values
-    vol_filter = volume > vol_ma
+    vol_spike = volume > (vol_ma * 1.5)  # Require 50% above average
     
     signals = np.zeros(n)
     position = 0
@@ -63,12 +63,12 @@ def generate_signals(prices):
             continue
         
         if position == 0:
-            # Long: break above R1 + above 1d EMA + volume
-            if close[i] > camarilla_r1_aligned[i] and close[i] > ema_1d_aligned[i] and vol_filter[i]:
+            # Long: break above R1 + above 1d EMA + volume spike
+            if close[i] > camarilla_r1_aligned[i] and close[i] > ema_1d_aligned[i] and vol_spike[i]:
                 signals[i] = 0.25
                 position = 1
-            # Short: break below S1 + below 1d EMA + volume
-            elif close[i] < camarilla_s1_aligned[i] and close[i] < ema_1d_aligned[i] and vol_filter[i]:
+            # Short: break below S1 + below 1d EMA + volume spike
+            elif close[i] < camarilla_s1_aligned[i] and close[i] < ema_1d_aligned[i] and vol_spike[i]:
                 signals[i] = -0.25
                 position = -1
         elif position == 1:
