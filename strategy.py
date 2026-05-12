5bf46759178f3cd8e4fc58b25115a85de57fc3bd
Tien Le <nhocconan@gmail.com>
2026-05-12 00:01:53 +0000

    exp#156065: 12h_Camarilla_R3_S3_Breakout_1dTrend_1dVolume

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R3_S3_Breakout_1dTrend_VolumeSpike"
-timeframe = "4h"
+name = "12h_Camarilla_R3_S3_Breakout_1dTrend_1dVolume"
+timeframe = "12h"
 leverage = 1.0
 
 import numpy as np
@@ -36,11 +36,11 @@ def generate_signals(prices):
     camarilla_h3 = close_1d + range_1d * 1.1 / 4
     camarilla_l3 = close_1d - range_1d * 1.1 / 4
     
-    # Align to 4h
+    # Align to 12h
     camarilla_h3_aligned = align_htf_to_ltf(prices, df_1d, camarilla_h3)
     camarilla_l3_aligned = align_htf_to_ltf(prices, df_1d, camarilla_l3)
     
-    # Session filter: London/NY overlap (08-16 UTC)
+    # Session filter: 00-12 UTC (first half of day) and 12-24 UTC (second half)
     hours = pd.DatetimeIndex(prices['open_time']).hour
     
     signals = np.zeros(n)
@@ -58,9 +58,9 @@ def generate_signals(prices):
                 signals[i] = 0.0
             continue
         
-        # Session filter: active during London/NY overlap (08-16 UTC)
+        # Session filter: active during London/NY overlap (08-16 UTC) and Asia (00-08 UTC)
         hour = hours[i]
-        in_session = (8 <= hour <= 16)
+        in_session = ((0 <= hour <= 8) or (8 <= hour <= 16))
         
         if not in_session:
             if position != 0:
