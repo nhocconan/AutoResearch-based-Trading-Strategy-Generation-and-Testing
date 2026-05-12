3b4c0e4c9ef3e235b387af895c9a57bb20922822
Tien Le <nhocconan@gmail.com>
2026-05-12 05:30:03 +0000

    exp#157602: 12h_Camarilla_R1_S1_Breakout_1wTrend_VolumeSpike

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R3_S3_Breakout_1wTrend_VolumeSpike"
-timeframe = "4h"
+name = "12h_Camarilla_R1_S1_Breakout_1wTrend_VolumeSpike"
+timeframe = "12h"
 leverage = 1.0
 
 import numpy as np
@@ -32,19 +32,18 @@ def generate_signals(prices):
     ema34_1w_aligned = align_htf_to_ltf(prices, df_1w, ema34_1w)
     
     # === Camarilla levels from previous 1d candle ===
-    # Calculate Camarilla levels using previous day's range
     prev_close_1d = np.roll(close_1d, 1)
     prev_high_1d = np.roll(high_1d, 1)
     prev_low_1d = np.roll(low_1d, 1)
     range_1d = prev_high_1d - prev_low_1d
     
-    # Camarilla levels: R3 = C + ((H-L) * 1.1/4), S3 = C - ((H-L) * 1.1/4)
-    r3 = prev_close_1d + (range_1d * 1.1 / 4)
-    s3 = prev_close_1d - (range_1d * 1.1 / 4)
+    # Camarilla levels: R1 = C + ((H-L) * 1.1/12), S1 = C - ((H-L) * 1.1/12)
+    r1 = prev_close_1d + (range_1d * 1.1 / 12)
+    s1 = prev_close_1d - (range_1d * 1.1 / 12)
     
-    # Align to 4h timeframe
-    r3_aligned = align_htf_to_ltf(prices, df_1d, r3)
-    s3_aligned = align_htf_to_ltf(prices, df_1d, s3)
+    # Align to 12h timeframe
+    r1_aligned = align_htf_to_ltf(prices, df_1d, r1)
+    s1_aligned = align_htf_to_ltf(prices, df_1d, s1)
     
     # === Volume spike detection ===
     vol_ma = pd.Series(volume).rolling(window=20, min_periods=20).mean().values
@@ -58,8 +57,8 @@ def generate_signals(prices):
     for i in range(start_idx, n):
         # Skip if data not ready
         if (np.isnan(ema34_1w_aligned[i]) or 
-            np.isnan(r3_aligned[i]) or
-            np.isnan(s3_aligned[i]) or
+            np.isnan(r1_aligned[i]) or
+            np.isnan(s1_aligned[i]) or
             np.isnan(vol_ma[i])):
             if position != 0:
                 signals[i] = 0.0
@@ -69,28 +68,28 @@ def generate_signals(prices):
             continue
         
         if position == 0:
-            # Long: price breaks above R3 + volume spike + 1w trend up
-            if (close[i] > r3_aligned[i] and 
+            # Long: price breaks above R1 + volume spike + 1w trend up
+            if (close[i] > r1_aligned[i] and 
                 volume_spike[i] and
                 close[i] > ema34_1w_aligned[i]):
                 signals[i] = 0.25
                 position = 1
-            # Short: price breaks below S3 + volume spike + 1w trend down
-            elif (close[i] < s3_aligned[i] and 
+            # Short: price breaks below S1 + volume spike + 1w trend down
+            elif (close[i] < s1_aligned[i] and 
                   volume_spike[i] and
                   close[i] < ema34_1w_aligned[i]):
                 signals[i] = -0.25
                 position = -1
         elif position == 1:
-            # Exit long: price crosses below S3 or trend breaks
-            if close[i] < s3_aligned[i] or close[i] < ema34_1w_aligned[i]:
+            # Exit long: price crosses below S1 or trend breaks
+            if close[i] < s1_aligned[i] or close[i] < ema34_1w_aligned[i]:
                 signals[i] = 0.0
                 position = 0
             else:
                 signals[i] = 0.25
         elif position == -1:
-            # Exit short: price crosses above R3 or trend breaks
-            if close[i] > r3_aligned[i] or close[i] > ema34_1w_aligned[i]:
+            # Exit short: price crosses above R1 or trend breaks
+            if close[i] > r1_aligned[i] or close[i] > ema34_1w_aligned[i]:
                 signals[i] = 0.0
                 position = 0
             else:
