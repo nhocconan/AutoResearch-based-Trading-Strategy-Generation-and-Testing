223f9b6b3d59381c8303bf033f92777c49fb81d1
Tien Le <nhocconan@gmail.com>
2026-05-12 02:43:33 +0000

    exp#156733: 4h_Camarilla_R3S3_Breakout_1dTrend_Volume_Confirmation

--- strategy.py
+++ strategy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-name = "12h_Camarilla_R3S3_Breakout_1dTrend_Volume_Confirmation"
-timeframe = "12h"
+name = "4h_Camarilla_R3S3_Breakout_1dTrend_Volume_Confirmation"
+timeframe = "4h"
 leverage = 1.0
 
 import numpy as np
@@ -32,7 +32,7 @@ def generate_signals(prices):
     r3 = p + (high_1d - low_1d) * 1.1 / 4
     s3 = p - (high_1d - low_1d) * 1.1 / 4
     
-    # Align Camarilla levels to 12h (wait for daily close)
+    # Align Camarilla levels to 4h (wait for daily close)
     r3_aligned = align_htf_to_ltf(prices, df_1d, r3)
     s3_aligned = align_htf_to_ltf(prices, df_1d, s3)
     
