df600b58f3b5165eec242478ccfb866408f0d1a9
Tien Le <nhocconan@gmail.com>
2026-05-12 02:41:53 +0000

    exp#156721: 4h_Camarilla_R3S3_Breakout_1dTrend_Volume_Spike_2

--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-name = "4h_Camarilla_R3S3_Breakout_1dTrend_Volume_Confirmation"
+name = "4h_Camarilla_R3S3_Breakout_1dTrend_Volume_Spike_2"
 timeframe = "4h"
 leverage = 1.0
 
